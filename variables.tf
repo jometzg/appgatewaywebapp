ff7d885046c786f22b6d20172f09d119529f9b9a
John Metzger <56820659+jometzg@users.noreply.github.com>
2020-06-17 10:24:52 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,105 @@
+variable "RESOURCE_GROUP_NAME" {
+  type    = string
+  default = "webapp-gateway-rg"
+}
+
+variable "LOCATION" {
+  type    = string
+  default = "westeurope"
+}
+
+variable "PREFIX" {
+  type    = string
+  default = "appgateway"
+}
+
+variable "HUB_VNET_ADDRESS_SPACE" {
+  type    = string
+  default = "172.0.0.0/16"
+}
+
+
+variable "HUB_DEFAULT_SUBNET_ADDRESS_PREFIX" {
+  type    = string
+  default = "172.0.0.0/24"
+}
+
+variable "HUB_FIREWALL_SUBNET_ADDRESS_PREFIX" {
+  type    = string
+  default = "172.0.1.0/24"
+}
+
+variable "HUB_WEBAPP_SUBNET_ADDRESS_PREFIX" {
+  type    = string
+  default = "172.0.2.0/24"
+}
+
+variable "WORKLOAD_VNET_ADDRESS_SPACE" {
+  type    = string
+  default = "172.1.0.0/16"
+}
+
+variable "WORKLOAD_DEFAULT_SUBNET_ADDRESS_PREFIX" {
+  type    = string
+  default = "172.1.0.0/24"
+}
+
+variable "WORKLOAD_GATEWAY_SUBNET_ADDRESS_PREFIX" {
+  type    = string
+  default = "172.1.1.0/24"
+}
+
+variable "WORKLOAD_WEBAPP_SUBNET_ADDRESS_PREFIX" {
+  type    = string
+  default = "172.1.2.0/24"
+}
+
+variable "WORKLOAD_BASTION_SUBNET_ADDRESS_PREFIX" {
+  type    = string
+  default = "172.1.3.0/24"
+}
+
+variable "IMAGE_REGISTRY_SERVER" {
+  type    = string
+  default = "YOUR_ACR.azurecr.io"
+}
+
+variable "IMAGE_REGISTRY_USERNAME" {
+  type    = string
+  default = "YOUR_ACT_USER"
+}
+
+variable "IMAGE_REGISTRY_PASSWORD" {
+  type    = string
+  default = "YOUR-ACR_KEY"
+}
+
+variable "CPU" {
+  type    = string
+  default = "1.0"
+}
+
+variable "MEMORY" {
+  type    = string
+  default = "4.0"
+}
+
+variable "DOCKER_IMAGE" {
+  type    = string
+  default = "YOUR_ACR.azurecr.io/webapplication3"
+}
+
+variable "DOCKER_PORT" {
+  type    = string
+  default = "80"
+}
+
+variable "WEBAPP_HOSTNAME" {
+  type    = string
+  default = "UNIQUE_NAME.azurewebsites.net"
+}
+
+variable "private_ip_address" {
+  description = "The Private IP Address to use for the Application Gateway."
+  default = "172.1.1.10"
+}
